2e137abef6f3300b5280bdf3c223ebc6649b5d87
Noah dela Cruz <delacruznoah12898@yahoo.com>
2024-12-29 15:28:46 -0800

    Add route to search for specific player

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import axios from "axios";
+import { BalldontlieAPI } from "@balldontlie/sdk";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 
+const api = new BalldontlieAPI({ apiKey: process.env.NBA_API_KEY });
+// const teams = await api.nba.getPlayers(); // https://api.balldontlie.io/v1/players?search=stephen
+// console.log(teams);
+
+app.get("/", (req, res) => {
+    res.send("Root");
+});
+
+app.get("/search/:name", async (req, res) => {
+    let { name } = req.params;
+    // let response = await axios.get(
+    //     `https://api.balldontlie.io/v1/players?search=${name}`,
+    //     {
+    //         headers: {
+    //             Authorization: process.env.NBA_API_KEY,
+    //         },
+    //     }
+    // );
+
+    // Search for players
+    const players = await api.nba.getPlayers({
+        search: name,
+        per_page: 100,
+    });
+    res.json(players);
+});
+
 const PORT = 3000;
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
