075c26b35d6cd6c4feebfe82f41f85c3476cf6e2
Noah dela Cruz <delacruznoah12898@yahoo.com>
2024-12-30 21:30:40 -0800

    Add toggle to favorite a player

--- frontend/components/PlayerCard.jsx
+++ frontend/components/PlayerCard.jsx
@@ -2,11 +2,20 @@ import { useLocation } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import { useState } from "react";
 
 export default function PlayerCard() {
     const location = useLocation();
     const playerData = location.state.playerData;
-    console.log(playerData);
+
+    const [favorite, setFavorite] = useState(false);
+
+    const handleClick = () => {
+        setFavorite(!favorite);
+    };
 
     const getOrdinal = (num) => {
         const suffixes = ["th", "st", "nd", "rd"];
@@ -29,7 +38,11 @@ export default function PlayerCard() {
                     <CardContent>
                         <Typography variant="h5" component="div">
                             {playerData.first_name} {playerData.last_name}
+                            <IconButton onClick={handleClick}>
+                                {!favorite ? <StarBorderIcon /> : <StarIcon />}
+                            </IconButton>
                         </Typography>
+
                         <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
                             {playerData.height}, {playerData.weight} lbs
                         </Typography>
